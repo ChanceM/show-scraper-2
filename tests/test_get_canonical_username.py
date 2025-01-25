877ea355b7d32e132f5c7c79b0373223943dcf90
Gregory Moore <grmoore18@gmail.com>
2025-01-24 21:51:51 -0800

    Fix get_canonical_username tests

--- tests/test_get_canonical_username.py
+++ tests/test_get_canonical_username.py
@@ -1,9 +1,11 @@
+import pytest
+from models.config import ConfigData
 from models.podcast import Person
 from scraper import get_canonical_username
 
-
-def test_known_usernames(mocker):
-    mocker.patch('scraper.config', {
+@pytest.fixture
+def mock_configuration(mocker):
+    return mocker.patch('scraper.CONFIGURATION', ConfigData(**{'shows': {}, 'data_dont_override': set(),
         'usernames_map': {
             'chris': {
                 'Chris Fisher'
@@ -15,29 +17,16 @@ def test_known_usernames(mocker):
                 ,'drewofdoom'
             }
         }
-    })
+    }))
 
+def test_known_usernames(mock_configuration):
     assert get_canonical_username(Person(name='Chris Fisher')) == 'chris'
     assert get_canonical_username(Person(name='chrislas')) == 'chris'
     assert get_canonical_username(Person(name='drewdevore')) == 'drew-devore'
     assert get_canonical_username(Person(name='drewdvore')) == 'drew-devore'
     assert get_canonical_username(Person(name='drewofdoom')) == 'drew-devore'
 
-def test_unknown_usernames(mocker):
-    mocker.patch('scraper.config', {
-        'usernames_map': {
-            'chris': {
-                'Chris Fisher'
-                ,'chrislas'
-            }
-            , 'drew-devore': {
-                'drewdevore'
-                ,'drewdvore'
-                ,'drewofdoom'
-            }
-        }
-    })
-
+def test_unknown_usernames(mock_configuration):
     assert get_canonical_username(Person(name='user')) == 'user'
     assert get_canonical_username(Person(name='user-01')) == 'user-01'
     assert get_canonical_username(Person(name='l33t~h@xor')) == 'l33t~h@xor'
