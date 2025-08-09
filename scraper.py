7244e364d3bd4250db610730f9fd84b7d7402953
Gregory Moore <grmoore18@gmail.com>
2025-08-08 22:18:48 -0700

    Add emoji_strip and tests for description parse

--- scraper.py
+++ scraper.py
@@ -306,7 +306,30 @@ def get_description(description: str) -> str:
         if element.string == ' ':
             continue
         description_parts.append(element.strip())
-    return normalize('NFKC',' '.join(description_parts))
+    return emoji_strip(normalize('NFKC',' '.join(description_parts)))
+
+def emoji_strip(description: str) -> str:
+    emoji_pattern = re.compile(
+        '^['
+        '\U0001F600-\U0001F64F'  # Emoticons
+        '\U0001F300-\U0001F5FF'  # Symbols & Pictographs
+        '\U0001F680-\U0001F6FF'  # Transport & Map Symbols
+        '\U0001F1E0-\U0001F1FF'  # Flags (iOS)
+        '\U0001FA70-\U0001FAFF'  # Symbols and Pictographs Extended-A
+        '\U00002702-\U000027B0'  # Dingbats
+        '\U000024C2-\U0001F251'  # Miscellaneous Symbols and Pictographs
+        r']+(\s+)?|(\s+)?['
+        '\U0001F600-\U0001F64F'  # Emoticons
+        '\U0001F300-\U0001F5FF'  # Symbols & Pictographs
+        '\U0001F680-\U0001F6FF'  # Transport & Map Symbols
+        '\U0001F1E0-\U0001F1FF'  # Flags (iOS)
+        '\U0001FA70-\U0001FAFF'  # Symbols and Pictographs Extended-A
+        '\U00002702-\U000027B0'  # Dingbats
+        '\U000024C2-\U0001F251'  # Miscellaneous Symbols and Pictographs
+        ']+$',
+        flags=re.UNICODE
+    )
+    return emoji_pattern.sub('', description)
 
 def build_participants(participants: List[Person]):
     for participant in list(filter(lambda person: person.role in [*Settings.Host_Roles, *Settings.Guest_Roles], participants)):
